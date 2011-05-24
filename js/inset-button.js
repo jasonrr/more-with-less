1836970b09306da1d208ec09721c3449dc7d5eab
Jason Rosoff <jasonrosoff@gmail.com>
2011-05-24 12:47:32 -0400

    Moved all button info into a mixin so it can be reused more easily. Semi-broken (only working with pre-compiled css)

--- js/inset-button.js
+++ js/inset-button.js
@@ -1,17 +1,25 @@
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? "rgba(" + parseInt(result["1"], 16) + ", " +
+                              parseInt(result["2"], 16) + ", " +
+                              parseInt(result["3"], 16) + ", 1)"
+    : null;
+}
+
 function isBackgroundDark(rgba) {
     var isDark = true;
     var rgbColor = rgba.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    var perceivedLuminance = 1 - (0.299 * 
-                                  rgbColor[1] + 0.587 * 
-                                  rgbColor[2] + 0.114 * 
-                                  rgbColor[3]) / 255;
+    if (!rgbColor) return;
+    var perceivedLuminance = 1 - (0.299 * rgbColor["1"] + 0.587 * rgbColor["2"] + 0.114 * rgbColor["3"]) / 255;
     
     if (perceivedLuminance < 0.30) isDark = false;
     return isDark;
 }
 
 $(function() {
-    if (!isBackgroundDark($('body').css('background-color'))) {
-        $('a.inset-button').removeClass('dark').addClass('light');
+    var rgba = $('body').css('background-color');
+    if (rgba.length == 7) rgba = hexToRgb(rgba);
+    if (!isBackgroundDark(rgba)) {
+        $('.dark').removeClass('dark').addClass('light');
     }
 });
